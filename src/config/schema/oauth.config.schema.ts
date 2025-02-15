68be5d7c292116f65a0d7fb9f17ac7c17b2def47
batuhan.balci <batuhanbalci@outlook.com>
2025-02-15 21:26:10 +0300

    feat: oauth config added

---
+++ src/config/schema/oauth.config.schema.ts
@@ -0,0 +1,12 @@
+import Joi from 'joi';
+
+export const oauthValidationSchema = Joi.object({
+  MICROSOFT_CLIENT_ID: Joi.string().optional(),
+  MICROSOFT_CLIENT_SECRET: Joi.string().optional(),
+  GOOGLE_CLIENT_ID: Joi.string().optional(),
+  GOOGLE_CLIENT_SECRET: Joi.string().optional(),
+  FACEBOOK_CLIENT_ID: Joi.string().optional(),
+  FACEBOOK_CLIENT_SECRET: Joi.string().optional(),
+  GITHUB_CLIENT_ID: Joi.string().optional(),
+  GITHUB_CLIENT_SECRET: Joi.string().optional(),
+});
