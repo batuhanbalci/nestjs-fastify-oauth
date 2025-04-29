459bf2e752eb2445f0a130bbcdbbfe5e68fb581f
batuhanbalci <batuhanbalci@outlook.com>
2025-04-29 10:09:17 +0300

    fix: module dependencies

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,14 +1,17 @@
 import { Module } from '@nestjs/common';
+import { HttpModule } from '@nestjs/axios';
 import { MailModule } from 'src/mail/mail.module';
 import { UsersModule } from 'src/users/users.module';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { JwtService } from './jwt.service';
+import { OAuthController } from './oauth.controller';
+import { OAuthService } from './oauth.service';
 
 @Module({
-  imports: [UsersModule, MailModule],
-  controllers: [AuthController],
-  providers: [AuthService, JwtService],
+  imports: [UsersModule, MailModule, HttpModule],
+  controllers: [AuthController, OAuthController],
+  providers: [AuthService, JwtService, OAuthService],
   exports: [JwtService],
 })
 export class AuthModule {}
